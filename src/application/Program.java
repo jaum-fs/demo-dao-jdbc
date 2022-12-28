59fb85441ecf847ff6ff04bfe94ab335aafbd844
jaum-fs <martins_joao@id.uff.br>
2022-12-27 23:38:29 -0300

    criando e implementando seller dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,7 @@ public class Program {
 		
 		Seller sl = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
+		SellerDao sDao = DaoFactory.createSellerDao();
 		
 		System.out.println(sl);
 
