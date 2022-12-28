3c3721739c10481464eb101c6e4b55b41472a552
jaum-fs <martins_joao@id.uff.br>
2022-12-28 00:51:55 -0300

    findById criado

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -95,8 +95,10 @@ public class Seller implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Seller [id = " + id + ", name = " + name + ", department = " + department.getName() + "]";
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
 	}
+
 	
 	
 	
